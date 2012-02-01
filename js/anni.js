20410923a925557f1cfe977b11a565db14470764
Nister <guanbeilang@gmail.com>
2012-02-01 11:36:14 +0800

    update statistic code

--- js/anni.js
+++ js/anni.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     $("section").hide();
     /* 增加百度统计代码输出 */
-    $.getScript("http://hm.baidu.com/h.js?8df3425a2bcb880e08debfb75995290f");
-    console.log(statistic);
+    $("<script></script>").attr({"src":"http://hm.baidu.com/h.js?8df3425a2bcb880e08debfb75995290f","type":"text/javascript"}).appendTo("body");
 });
