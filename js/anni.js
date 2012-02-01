31588290b35b4f2dd86a42b7a718d86a2ef90809
Nister <guanbeilang@gmail.com>
2012-02-01 11:44:36 +0800

    same to former

--- js/anni.js
+++ js/anni.js
@@ -1,5 +1,3 @@
 $(document).ready(function(){
     $("section").hide();
-    /* 增加百度统计代码输出 */
-    $("<script></script>").attr({"src":"http://hm.baidu.com/h.js?8df3425a2bcb880e08debfb75995290f","type":"text/javascript"}).appendTo("body");
 });
